0950cbea67574099f8c335c4991e4e669bfde3ba
utof0x <popovychmykhailo00@gmail.com>
2023-09-04 17:38:40 +0300

    product images section markup

--- resources/views/livewire/app/product.blade.php
+++ resources/views/livewire/app/product.blade.php
@@ -1,5 +1,38 @@
 <div class="flex">
-    <div>images</div>
+    <div>
+        <div class="rounded-lg overflow-hidden">
+            <img class="w-[35.5rem] h-[35.5rem]"
+                 src="{{ url('assets/images/product-not-found.png') }}"
+                 alt="product image"
+            >
+        </div>
+        <div class="flex justify-between p-6">
+            <div class="rounded-lg overflow-hidden">
+                <img class="w-28 h-28"
+                     src="{{ url('assets/images/product-not-found.png') }}"
+                     alt="product image"
+                >
+            </div>
+            <div class="rounded-lg overflow-hidden">
+                <img class="w-28 h-28"
+                     src="{{ url('assets/images/product-not-found.png') }}"
+                     alt="product image"
+                >
+            </div>
+            <div class="rounded-lg overflow-hidden">
+                <img class="w-28 h-28"
+                     src="{{ url('assets/images/product-not-found.png') }}"
+                     alt="product image"
+                >
+            </div>
+            <div class="rounded-lg overflow-hidden">
+                <img class="w-28 h-28"
+                     src="{{ url('assets/images/product-not-found.png') }}"
+                     alt="product image"
+                >
+            </div>
+        </div>
+    </div>
     <div>
         <div></div>
         <div></div>
@@ -7,4 +40,6 @@
         <div></div>
         <div></div>
     </div>
+</div>
+
 </div>
